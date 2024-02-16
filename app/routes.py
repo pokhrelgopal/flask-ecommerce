10531118fdaef34c54fd2a04ea1d198ae24d105b
Gopal Pokhrel <pokhrelgopal27@gmail.com>
2024-02-16 16:06:11 +0545

    working on it

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from app import app, db
-from app.models import User
+from app.models import User, Product
 from functools import wraps
 from werkzeug.utils import secure_filename
 import os
 
 UPLOAD_FOLDER = os.path.join("media", "uploads")
-ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "webp"}
 
 
 def allowed_file(filename):
@@ -84,7 +84,12 @@ def index():
 @app.route("/products", methods=["GET", "POST"])
 def products():
     status = is_authenticated()
-    return render_template("products.html", status=status)
+    q = request.args.get("q")
+    if q:
+        products = Product.query.filter(Product.name.contains(q)).all()
+        return render_template("products.html", status=status, products=products)
+    products = Product.query.all()
+    return render_template("products.html", status=status, products=products)
 
 
 @app.route("/cart", methods=["GET", "POST"])
@@ -135,7 +140,65 @@ def admin_products():
     if not status:
         flash("Please login to continue.", "warning")
         return redirect(url_for("login"))
-    return render_template("admin/products.html")
+
+    products = Product.query.all()
+
+    if request.method == "POST":
+        name = request.form.get("product_name")
+        price = request.form.get("product_price")
+        image_file = request.files["product_image"]
+        description = request.form.get("product_description")
+
+        if not name:
+            flash("Product name is required.", "warning")
+            return redirect(url_for("admin_products"))
+
+        if not price:
+            flash("Product price is required.", "warning")
+            return redirect(url_for("admin_products"))
+
+        if not description:
+            flash("Product description is required.", "warning")
+            return redirect(url_for("admin_products"))
+
+        if not image_file:
+            flash("Product image is required.", "warning")
+            return redirect(url_for("admin_products"))
+
+        if image_file and allowed_file(image_file.filename):
+            filename = secure_filename(image_file.filename)
+            image_file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            image_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+        else:
+            flash("Invalid file format for image.", "warning")
+            return redirect(url_for("admin_products"))
+
+        product = Product(
+            name=name, price=price, image=image_path, description=description
+        )
+        db.session.add(product)
+        db.session.commit()
+        flash("Product added successfully.", "success")
+        return redirect(url_for("admin_products"))
+
+    return render_template("admin/products.html", products=products)
+
+
+@app.route("/delete-product/", methods=["GET", "POST"])
+def delete_product():
+    status = is_authenticated()
+    if not status:
+        flash("Please login to continue.", "warning")
+        return redirect(url_for("login"))
+
+    if request.method == "POST":
+        product_id = request.form.get("product_id")
+        product = Product.query.filter_by(id=product_id).first()
+        db.session.delete(product)
+        db.session.commit()
+        flash("Product deleted successfully.", "success")
+
+    return redirect(url_for("admin_products"))
 
 
 @app.route("/admin-orders", methods=["GET", "POST"])
